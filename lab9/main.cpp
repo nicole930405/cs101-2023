47cdae28509b0907448dd24d0d80a6b0db3943d2
B1129041 <119797293+nicole930405@users.noreply.github.com>
2023-05-25 14:26:31 +0800

    Create main.cpp

---
+++ lab9/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class ReplaceMyString {
+    public:
+    ReplaceMyString() {}
+    void replaceString(string needle, string stick) {
+        ifstream in("main.cpp");
+        ofstream out("rmain.cpp");
+        char c;
+        while (true) {
+            in.get(c);
+            if (in.eof()) {
+                break;
+            }
+            if (c == needle[0]) {
+                for (int i=1; i<needle.length(); i++) {
+                    in >> c;
+                    if (c != needle[i]) {
+                        break;
+                    }
+                    if (i == needle.length()-1) {
+                        for (int j=0; j<stick.length(); j++) {
+                            out << stick[j];
+                        }
+                    }
+                }
+            } else {
+                out << c;
+            }
+        }
+        in.close();
+        out.close();
+    }
+};
+
+int main() {
+    ReplaceMyString my;
+    my.replaceString("IU", "IU is best");
+    return 0;
+}
