78d90da793213dd0cd18445cc44ddfbb25fbcd44
B1129041 <119797293+nicole930405@users.noreply.github.com>
2023-06-01 14:32:51 +0800

    Create main.cpp

---
+++ lab10/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class mystring {
+    private:
+        string m_str;
+    public:
+        mystring(string s) {
+            m_str = s;
+        }
+};
+class readClass {
+    public:
+    string line;
+    int x = 0;
+    void showClass(){
+        ifstream in;
+        string line;
+        in.open("main.cpp");
+        while(getline(in, line)) {
+            if (line.find("x",0)){
+                x++;
+            }
+        }
+        in.close();
+        cout << x;
+    }
+};
+int main(){
+    readClass rfile;
+    rfile.showClass();
+    return 0;
+}
